ab22fa8afbf2ae2ab7f14c9e739a2f3dbdbb49b6
shubha-mm <167985010+shubha-mm@users.noreply.github.com>
2024-11-15 22:30:52 +0530

    project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  isLandingPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      // Check if the current route is the landing page
+      this.isLandingPage = this.router.url === '/landing';
+    });
+  }
 }
